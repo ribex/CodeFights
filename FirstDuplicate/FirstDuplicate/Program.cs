d4c287ed416fc72934c860d3b7640debfb4c3d20
ribex <ribexy@gmail.com>
2017-12-24 12:50:50 -0500

    Another attempt, fails 2 tests

--- FirstDuplicate/FirstDuplicate/Program.cs
+++ FirstDuplicate/FirstDuplicate/Program.cs
@@ -29,29 +29,54 @@ namespace FirstDuplicate
 //            Console.WriteLine((lowestIndex < a.Length) ? a[lowestIndex] : -1);
 //        }
 
+//        static void Main(string[] args)
+//        {
+//            //int[] a = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 2, 3, 4, 5, 7, 9 };
+//            int[] a = { 1, 2, 3, 4, 5, 4, 3, 2, 1 };
+//            int[] dupInts = new int[a.Length];
+//            var count = 0;
+//            var lowestIndex = a.Length;
+//
+//
+//            for (var candidate = 0; candidate < lowestIndex - 1; candidate++)
+//            {
+//                if (Array.IndexOf(dupInts, a[candidate]) != -1)
+//                {
+//                    break;
+//                }
+//                var index = Array.IndexOf(a, a[candidate], candidate + 1);
+//                if (index < lowestIndex && index != -1)
+//                {
+//                    lowestIndex = index;
+//                }
+//                dupInts[count] = a[candidate];
+//            }
+//            Console.WriteLine((lowestIndex < a.Length) ? a[lowestIndex] : -1);
+//        }
+
         static void Main(string[] args)
         {
             //int[] a = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 2, 3, 4, 5, 7, 9 };
             int[] a = { 1, 2, 3, 4, 5, 4, 3, 2, 1 };
             int[] dupInts = new int[a.Length];
-            var count = 0;
-            var lowestIndex = a.Length;
+            var index = 0;
+            var size = a.Length;
 
-
-            for (var candidate = 0; candidate < lowestIndex - 1; candidate++)
+            for (var candidate = 0; candidate < size; candidate++)
             {
-                if (Array.IndexOf(dupInts, a[candidate]) != -1)
+                // if the candidate value is not in the new array
+                if (Array.IndexOf(dupInts, a[candidate]) == -1)
                 {
-                    break;
+                    dupInts[index] = a[candidate];
+                    index++;
                 }
-                var index = Array.IndexOf(a, a[candidate], candidate + 1);
-                if (index < lowestIndex && index != -1)
+                else
                 {
-                    lowestIndex = index;
+                    Console.WriteLine(a[candidate]);
                 }
-                dupInts[count] = a[candidate];
+
             }
-            Console.WriteLine((lowestIndex < a.Length) ? a[lowestIndex] : -1);
+
         }
     }
 }
