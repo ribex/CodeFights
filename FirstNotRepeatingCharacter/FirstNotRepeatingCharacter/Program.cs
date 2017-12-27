a97a2c8f87dbb698cdcbace3c76744adf2840634
Rebecca Wagaman <rebecca.wagaman@gmail.com>
2017-12-27 10:33:53 -0500

    Comment

--- FirstNotRepeatingCharacter/FirstNotRepeatingCharacter/Program.cs
+++ FirstNotRepeatingCharacter/FirstNotRepeatingCharacter/Program.cs
@@ -8,7 +8,8 @@ namespace FirstNotRepeatingCharacter
 {
     class Program
     {
-        // Write a solution that only iterates over the string once and uses O(1) additional memory
+//      Write a solution that only iterates over the string once and uses O(1) additional memory
+//      Given a string s, find and return the first instance of a non-repeating character in it.If there is no such character, return '_'.
         static void Main(string[] args)
         {
             var s = "abacabad";
