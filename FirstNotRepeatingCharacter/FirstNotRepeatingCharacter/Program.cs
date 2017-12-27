85a98640c0eec5d069940c62be87a2a854ec0ae6
Rebecca Wagaman <rebecca.wagaman@gmail.com>
2017-12-26 20:19:28 -0500

    Add comment

--- FirstNotRepeatingCharacter/FirstNotRepeatingCharacter/Program.cs
+++ FirstNotRepeatingCharacter/FirstNotRepeatingCharacter/Program.cs
@@ -8,10 +8,10 @@ namespace FirstNotRepeatingCharacter
 {
     class Program
     {
+        // Write a solution that only iterates over the string once and uses O(1) additional memory
         static void Main(string[] args)
         {
             var s = "abacabad";
-
 
 
         }
