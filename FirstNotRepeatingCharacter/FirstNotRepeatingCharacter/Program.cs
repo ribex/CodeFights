6fafc3c9a86b97e5c0dabbc4d2dea7a05bb8bbb2
Rebecca Wagaman <rebecca.wagaman@gmail.com>
2018-01-03 19:40:58 -0500

    Pseudocode

--- FirstNotRepeatingCharacter/FirstNotRepeatingCharacter/Program.cs
+++ FirstNotRepeatingCharacter/FirstNotRepeatingCharacter/Program.cs
@@ -23,6 +23,8 @@ namespace FirstNotRepeatingCharacter
             for (var i = 0; i < len; i++)
             {
                 // if character exists in hash
+
+                // else
             }
             // no such character
             Console.WriteLine("_");
